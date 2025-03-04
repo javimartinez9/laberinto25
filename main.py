649b558cbb12a5e0f643c5edf939b34dc4088221
javimartinez9 <javiermartineznieva@gmail.com>
2025-03-04 14:24:43 +0100

    Añadimos el patron composite con sus respectivas clases

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from creator import Creator,CreatorB 
+from creator import Creator, CreatorB
 from juego import Juego
 #ejemplo de uso
 fm = Creator()
@@ -17,6 +17,7 @@ hab2=juego.obtenerHabitacion(2)
 print(hab1.norte.activa)
 print(hab2.sur.activa)
 
+
 # Crear laberinto de 4 habitaciones
 fm = Creator()
 juego.laberinto = juego.crearLaberinto4Hab(fm)
@@ -26,6 +27,35 @@ for habitacion in juego.laberinto.habitaciones:
     print(f"Habitación {habitacion.num}")
     if hasattr(habitacion, 'bicho'):
         bicho = habitacion.bicho
+        bicho = habitacion.bicho
+from creator import Creator, CreatorB
+from juego import Juego, Bomba
+#ejemplo de uso
+fm = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(fm)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+
+#laberinto con paredes bomba
+fmb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(fmb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+# Crear laberinto de 4 habitaciones
+fm = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(fm)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
         bicho = habitacion.bicho
 from creator import Creator, CreatorB
 from juego import Juego, Bomba
